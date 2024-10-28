d7d9c27df474c07b89f4827107a0293874c25415
thomas bertok <thomasbertok@gmail.com>
2024-10-28 19:38:34 +0200

    add google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import ReactGA from "react-ga4";
 import { AppContextProvider } from "@/context/AppContext";
 import { PlayerContextProvider } from "./context/PlayerContext";
 import Sections from "@/sections/Sections";
 import "@/styles/App.css";
+
+const TRACKING_ID = import.meta.env.VITE_GOOGLE_ANALYTICS_ID;
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
   return (
