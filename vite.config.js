0edde297f674f83028525f54722602f2a7389696
thomas bertok <thomasbertok@gmail.com>
2026-05-17 17:13:08 +0300

    version updates, and fixes

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://react-svgr.com/docs/options/
 const svgrOptions = {
@@ -15,7 +16,7 @@ const svgrOptions = {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(svgrOptions)],
+  plugins: [react(), svgr(svgrOptions), tailwindcss()],
   resolve: {
     alias: {
       "@": "/src",
